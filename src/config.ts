4db9a3e08bbeed0895a451c58ed369ac88567fff
Alfred Coloma <SrColoma.oficial@gmail.com>
2023-02-09 21:30:18 -0500

    mas cositas

--- src/config.ts
+++ src/config.ts
@@ -1,33 +1,47 @@
 export const SITE = {
-  title: "Astro Design System",
-  description: "Astro theme to build your design system fast.",
-  defaultLanguage: "en_US",
-  twitter: "@jordienr",
-  github: "jordienr",
-  linkedin: "linkedinUsername",
+  title: "FCI integracion",
+  description: "Integracion de los proyectos fci 2022-2023.",
+  defaultLanguage: "en_ES",
+  // twitter: "@SrColoma",
+  // github: "SrColoma",
+  // linkedin: "alfred-coloma-ortiz-8938b2173",
 };
 
 export const OPEN_GRAPH = {
   image: {
-    src: "",
+    src: "/public/core.jpg",
     alt: "",
   },
   twitter: "",
 };
 
 export const SIDEBAR = [
-  { text: "Core", header: true },
+  // { text: "Core", header: true },  
   { text: "Introduction", link: "/core/introduction" },
   { text: "Colors", link: "/core/colors" },
   { text: "Typography", link: "/core/typography" },
   { text: "Shadows", link: "/core/shadows" },
 
-  { text: "Components", header: true },
+  // { text: "Components", header: true },
   { text: "Buttons", link: "/components/buttons" },
   { text: "Input", link: "/components/input" },
   { text: "Status pills", link: "/components/status-pill" },
   { text: "Table", link: "/components/table" },
 
-  { text: "Patterns", header: true },
+  // { text: "Patterns", header: true },
   { text: "Introduction", link: "/patterns/introduction" },
+
+  { text: "Camaron", header: true },
+  { text: "Proyecto", link: "/camaron/camaron" },
+  { text: "Tesis", link: "/camaron/camaron" },
+  { text: "Manual", link: "/camaron/camaron" },
+
+  { text: "Cacao", header: true },
+  { text: "Cacao", link: "/camaron/camaron" },
+
+  { text: "Cafe", header: true },
+  { text: "Camaron", link: "/camaron/camaron" },
+
+  { text: "Tilapia", header: true },
+  { text: "Camaron", link: "/camaron/camaron" },
 ];
---
+++ src/pages/camaron/camaron.md
@@ -0,0 +1,16 @@
+---
+layout: ../../layouts/MainLayout.astro
+---
+
+# Camaron
+
+Cosas de camarones
+<div class="component-preview">
+    <div class="inline-grid grid-cols-2 gap-16">
+        <!-- cuatro imagenes de /public -->
+        <img src="/public/camaron.png" alt="camaron 1" class="w-full">
+        <img src="/public/camaron.png" alt="camaron 2" class="w-full">
+        <img src="/public/camaron.png" alt="camaron 3" class="w-full">
+        <img src="/public/camaron.png" alt="camaron 4" class="w-full">
+    </div>
+</div>
